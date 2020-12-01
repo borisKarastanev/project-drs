4aeb2a275ecbc36888e3edf0528a6eb5678db22f
Boris Karastanev <b.karastanev@gmail.com>
2020-12-01 22:25:18 +0200

    Removed commented out code

--- main.py
+++ main.py
@@ -4,10 +4,6 @@
 from servo import Servo
 
 try:
-    # pwm = servo.setup()
-    # servo.enable_drs(pwm)
-    # servo.disable_drs(pwm)
-    # servo.stop(pwm)
     servo = Servo()
     servo.enable_drs()
     servo.disable_drs()
@@ -15,3 +11,4 @@ try:
 
 except Exception as error:
     print("DRS Failed", error)
+    
